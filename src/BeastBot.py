6192e0dc79d90a5187f14854ecf1dbd945a6d506
Jacques Pharand <jacquespharand1@gmail.com>
2015-03-28 16:32:51 -0400

    Initial Commit

---
+++ src/BeastBot.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#   BeastBot.py - A custom, from scratch IRC bot
+#   Copyright (C) 2015 Spacecow99
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import re
+import sys
+import socket
+import os.path
+import logging
+import argparse
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
