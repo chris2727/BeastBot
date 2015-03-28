ad4a8b5bcc9ab01c41d7b899083d6e46978cc35a
chris <christopherddurhamyahoo@gmail.com>
2015-03-28 14:37:38 -0800

    added basic connection functionality

--- src/BeastBot.py
+++ src/BeastBot.py
@@ -24,11 +24,45 @@ import socket
 import os.path
 import logging
 import argparse
-
+import ConfigParser
+import re
 
 def main():
-    pass
+    irc = CreateSocket()
+    conf = getConfig()
+    while True:
+	try:
+		line = irc.recv(2048)
+	except:
+		print "IRC Socket Disconnected"
+		wait = input('Press enter to continue')
+		exit()
+	if line:
+		print line
+	splitline = line.split(" :")
+	if splitline[0] == "PING":
+		pong = "PONG %s" %splitline[1]
+		irc.send(pong)
+		print pong
+	if re.match(":r2d2.evilzone.org 001 "+conf['nick']+" :Welcome", line):
+		splitchannels = conf['channels'].split(" ")
+		for chan in splitchannels:
+			irc.send("JOIN %s\n" %(chan))
 
+def CreateSocket():
+	conf = getConfig()
+	irc = socket.socket ( socket.AF_INET, socket.SOCK_STREAM )
+	irc.connect((conf['server'], int(conf['port'])))
+	irc.send('NICK '+conf['nick']+'\r\n')
+	irc.send('USER test test test :test\r\n')
+	return irc
+
+def getConfig():
+	config = ConfigParser.RawConfigParser()
+	config.read('conf/beastbot.conf')
+	conf = dict(config.items('Main'))
+	return conf
 
 if __name__ == "__main__":
     main()
+
