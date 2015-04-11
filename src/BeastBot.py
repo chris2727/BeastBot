18d8bc028712ca2e222c7316e7c6224571298b1d
chris <christopherddurhamyahoo@gmail.com>
2015-04-10 18:59:17 -0800

    fixing bugs

--- src/BeastBot.py
+++ src/BeastBot.py
@@ -24,12 +24,106 @@ import os
 from threading import Thread
 import thread
 
-
-def main():
-    irc = mainFunc.CreateSocket()
+def parsecommand(line, irc):
     conf = mainFunc.getConfig()
     functions = mainFunc.getFunctions()
     serverfunctions = mainFunc.getServerFunctions()
+    if line:
+        print line
+    splitline = line.split(" :")
+    try:
+        message = splitline[1]
+        origmessage = message
+        messagechars = message
+        message = message.split(" ")
+        username = line.split("!")[0].replace(':', '')
+        msgto = line.split(" ")[2]  # Gives an error into the log
+        if msgto == conf['nick']:
+            msgto = username
+        command = message[0]
+        chan = msgto
+    except IndexError, e:
+        errorhandling.errorlog('information', e, line)
+    except Exception, e:
+        errorhandling.errorlog('critical', e)
+    try:
+        if re.match("http", message[0]):
+            message[0] = conf['cominit']+"http"
+            command = conf['cominit']+"http"
+            messagechars = message[0]
+    except IndexError, e:
+        errorhandling.errorlog('information', e)
+
+    if messagechars[0] == conf['cominit']:
+        command = command[1:]
+        command = command.strip()
+        if command == "reload":
+            if username in conf['admins'].split(" "):
+                if (ircFunc.isRegged(username, irc)):
+                    mainFunc.reloadImports()
+                    conf = mainFunc.getConfig()
+                    functions = mainFunc.getFunctions()
+                    modules = mainFunc.getModules()
+                    ircFunc.ircSay(msgto, "Configuration Reloaded...", irc)
+        elif command == "quit":
+            if username in conf['admins'].split(" "):
+                if (ircFunc.isRegged(username, irc)):
+                    ircFunc.ircSay(msgto, "Shutting Down....", irc)
+                    mainFunc.cleanConfig()
+                    irc.close()
+                    time.sleep(1)
+                    exit()
+        elif command == "update":
+            if username in conf['admins'].split(" "):
+                if (ircFunc.isRegged(username, irc)):
+                    msg = "Shutting down for updates..."
+                    ircFunc.ircSay(msgto, msg, irc)
+                    mainFunc.cleanConfig()
+                    try:
+                        irc.close()
+                    except Exception, e:
+                        ircSay(username, 'Forcing Shutdown...', irc)
+                        errorhandling.errorlog('critical', e, 'forcing shutdown')
+                        exit()
+                    time.sleep(3)
+                    os.system("python pullupdates.py")
+                    # Bot should not go past this line
+                    msg = "Error pulling updates..."
+                    errorhandling.errorlog('critical', msg)
+                    exit()
+        else:
+            try:
+                if functions[command]:
+                    function = functions[command]
+                    functionThread = Thread(target=eval(function), args=(line, irc,))
+                    functionThread.daemon = True
+                    functionThread.start()
+            except KeyError:
+                #Command does not exist
+                pass
+            except Exception, e:
+                errorhandling.errorlog('critical', e)
+    else:
+        try:
+            init = splitline[0]
+            init = init.lower()
+            if serverfunctions[init]:
+                function = serverfunctions[init]
+                eval(function)(line, irc)
+        except KeyError:
+            #command does not exist
+            pass
+        except Exception, e:
+            errorhandling.errorlog('critical', e)
+    if re.match("^:[a-zA-Z0-9\-\.]+\.[a-zA-Z0-9\-\.]+\.(com|org|net|mil|edu|COM|ORG|NET|MIL|EDU) 001 "+conf['nick']+" :Welcome", line):
+        splitchannels = conf['channels'].split(" ")
+        for chan in splitchannels:
+            irc.send("JOIN %s\n" % (chan))
+    elif re.search(":This nickname is registered and protected.", line):
+        ircFunc.ircSay("NICKSERV", "identify "+conf['password'], irc)
+
+def main():
+    irc = mainFunc.CreateSocket()
     while True:
         try:
             line = irc.recv(2048)
@@ -37,100 +131,10 @@ def main():
             print "IRC Socket Disconnected"
             wait = input('Press enter to continue')
             exit()
-        if line:
-            print line
-        splitline = line.split(" :")
-        try:
-            message = splitline[1]
-            origmessage = message
-            messagechars = message
-            message = message.split(" ")
-            username = line.split("!")[0].replace(':', '')
-            msgto = line.split(" ")[2]  # Gives an error into the log
-            if msgto == conf['nick']:
-                msgto = username
-            command = message[0]
-            chan = msgto
-        except IndexError, e:
-            errorhandling.errorlog('information', e, line)
-        except Exception, e:
-            errorhandling.errorlog('critical', e)
-        try:
-            if re.match("http", message[0]):
-                message[0] = conf['cominit']+"http"
-                command = conf['cominit']+"http"
-                messagechars = message[0]
-        except IndexError, e:
-            errorhandling.errorlog('information', e)
-
-        if messagechars[0] == conf['cominit']:
-            command = command[1:]
-            command = command.strip()
-            if command == "reload":
-                if username in conf['admins'].split(" "):
-                    if (ircFunc.isRegged(username, irc)):
-                        mainFunc.reloadImports()
-                        conf = mainFunc.getConfig()
-                        functions = mainFunc.getFunctions()
-                        modules = mainFunc.getModules()
-                        ircFunc.ircSay(msgto, "Configuration Reloaded...", irc)
-            elif command == "quit":
-                if username in conf['admins'].split(" "):
-                    if (ircFunc.isRegged(username, irc)):
-                        ircFunc.ircSay(msgto, "Shutting Down....", irc)
-                        mainFunc.cleanConfig()
-                        irc.close()
-                        time.sleep(1)
-                        exit()
-            elif command == "update":
-                if username in conf['admins'].split(" "):
-                    if (ircFunc.isRegged(username, irc)):
-                        msg = "Shutting down for updates..."
-                        ircFunc.ircSay(msgto, msg, irc)
-                        mainFunc.cleanConfig()
-                        try:
-                            irc.close()
-                        except Exception, e:
-                            ircSay(username, 'Forcing Shutdown...', irc)
-                            errorhandling.errorlog('critical', e, 'forcing shutdown')
-                            exit()
-                        time.sleep(3)
-                        os.system("python pullupdates.py")
-                        # Bot should not go past this line
-                        msg = "Error pulling updates..."
-                        errorhandling.errorlog('critical', msg)
-                        exit()
-            else:
-                try:
-                    if functions[command]:
-                        function = functions[command]
-                        functionThread = Thread(target=eval(function), args=(line, irc,))
-                        functionThread.daemon = True
-                        functionThread.start()
-                except KeyError:
-                    #Command does not exist
-                    pass
-                except Exception, e:
-                    errorhandling.errorlog('critical', e)
-        else:
-            try:
-                init = splitline[0]
-                init = init.lower()
-                if serverfunctions[init]:
-                    function = serverfunctions[init]
-                    eval(function)(line, irc)
-            except KeyError:
-                #command does not exist
-                pass
-            except Exception, e:
-                errorhandling.errorlog('critical', e)
-        if re.match("^:[a-zA-Z0-9\-\.]+\.[a-zA-Z0-9\-\.]+\.(com|org|net|mil|edu|COM|ORG|NET|MIL|EDU) 001 "+conf['nick']+" :Welcome", line):
-            splitchannels = conf['channels'].split(" ")
-            for chan in splitchannels:
-                irc.send("JOIN %s\n" % (chan))
-        elif re.search(":This nickname is registered and protected.", line):
-            ircFunc.ircSay("NICKSERV", "identify "+conf['password'], irc)
-
+        runThread = Thread(target=parsecommand, args=(line, irc,))
+        runThread.daemon = True
+        runThread.start()
+        
 
 def cleanConfig():
     config = ConfigParser.RawConfigParser()
