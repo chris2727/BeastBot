75c5be48ad62a48f7f99ecdfe3fde81dc8ce721b
kulverstukas <kulverstukas.k@gmail.com>
2015-05-04 22:10:20 +0300

    Added slap.py

---
+++ src/modules/__init__.py
@@ -0,0 +1,1 @@
+__all__ =['announce', 'botadministration', 'channeladministration', 'errorhandling', 'information', 'ircFunc', 'isup', 'mainFunc', 'ruby', 'slap', 'testmodule', 'urban', 'urlparsing']
